53ba5c4907e7a1ff57097f598c78c083c99dd566
aldo <muhammadrifaldosaputra@gmail.com>
2025-03-05 20:33:32 +0700

    day 1

---
+++ latihan_java/src/latihan_java/Latihan_java.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_java;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // menampilkan pesan pertama di java
+        System.out.println("hai ini adalah kode pertama saya");
+        
+        //println akan membuat baris baru sedangkan print akan mencetak tanpa membuat baris baru
+        System.out.print("pesan kedua dan ");
+        System.out.println("pesan ketiga");
+    }
+    
+}
